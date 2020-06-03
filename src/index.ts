09f07db808a4ce065679b691ba341abe9803acf5
Hugo Ferreira <bytter@gmail.com>
2020-06-03 02:30:53 +0100

    Co-authored-by: up353972@ms.uporto.pt <up353972@ms.uporto.pt>
    We now support composition and referecing of arbitraries. However, this version is highly type-unsafe.

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,30 @@
 import * as fc from 'fast-check'
 
-export class CucumberCheck<T> {
-    properties: Array<(tc: T) => void> = []
+export abstract class CucumberCheck {
+    arbitraries: Map<string, fc.Arbitrary<any>> = new Map
+    properties: Map<string, Array<(tc: any) => void>> = new Map
 
-    constructor(public suite: fc.Arbitrary<T>) { }
+    abstract initialize(): void
 
-    initialize(tc: T) {
-        this.properties.forEach(f => f(tc))
+    arbitrary<U>(name: string, a: fc.Arbitrary<U>) {
+        this.arbitraries.set(name, a)
     }
 
-    register(f: (_: T) => void) {
-        this.properties.push(f)
+    property<U>(name: string, f: (out: this, _: U) => void) {
+        if (!this.properties.has(name)) this.properties.set(name, new Array)
+        this.properties.get(name)!.push(a => f(this, a))
     }
 
-    assert(f: (_: T) => void) {
-        fc.assert(fc.property(this.suite, tc => {
-            this.initialize(tc)
-            f(tc)
-        }))
+    assert(assertion: (out: this, _: any) => void) {
+        const obj = [...this.arbitraries.entries()].reduce((obj, [key, value]) => { (obj as any)[key] = value; return obj }, {})
+        const suite = fc.record(obj)
+
+        fc.assert(fc.property(suite, tc => {
+            this.initialize();
+            [...this.properties.entries()].forEach(([k, p]) => {
+                if (k in tc) p.forEach(f => f((tc as any)[k]))
+            })
+            assertion(this, tc)
+        })) 
     }
 }
