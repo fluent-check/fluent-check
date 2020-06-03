9ea2e3b4db3d9f8f63612f0ac3069bfb2693b406
Andre Restivo <andre.restivo@gmail.com>
2020-06-03 02:58:30 +0100

    Made arbitraries and properties private

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
 import * as fc from 'fast-check'
 
 export abstract class CucumberCheck {
-    arbitraries: Map<string, fc.Arbitrary<any>> = new Map
-    properties: Map<string, Array<(tc: any) => void>> = new Map
+    #arbitraries: Map<string, fc.Arbitrary<any>> = new Map
+    #properties: Map<string, Array<(tc: any) => void>> = new Map
 
     abstract initialize(): void
 
     arbitrary<U>(name: string, a: fc.Arbitrary<U>) {
-        this.arbitraries.set(name, a)
+        this.#arbitraries.set(name, a)
     }
 
     property<U>(name: string, f: (out: this, _: U) => void) {
-        if (!this.properties.has(name)) this.properties.set(name, new Array)
-        this.properties.get(name)!.push(a => f(this, a))
+        if (!this.#properties.has(name)) this.#properties.set(name, new Array)
+        this.#properties.get(name)!.push(a => f(this, a))
     }
 
     assert(assertion: (out: this, _: any) => void) {
-        const obj = [...this.arbitraries.entries()].reduce((obj, [key, value]) => { (obj as any)[key] = value; return obj }, {})
+        const obj = [...this.#arbitraries.entries()].reduce((obj, [key, value]) => { (obj as any)[key] = value; return obj }, {})
         const suite = fc.record(obj)
 
         fc.assert(fc.property(suite, tc => {
             this.initialize();
-            [...this.properties.entries()].forEach(([k, p]) => {
+            [...this.#properties.entries()].forEach(([k, p]) => {
                 if (k in tc) p.forEach(f => f((tc as any)[k]))
             })
             assertion(this, tc)
