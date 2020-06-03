0146388a8d6fac501854c587f08ce3380559dc40
Andre Restivo <andre.restivo@gmail.com>
2020-06-03 14:08:50 +0100

    Removed necessity to pass arbitrary name from property

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as fc from 'fast-check'
 
 export abstract class CucumberCheck {
     #arbitraries: Map<string, fc.Arbitrary<any>> = new Map
-    #properties: Map<string, Array<(tc: any) => void>> = new Map
+    #properties: Array<(sut: this, tc: any) => void> = []
 
     abstract initialize(): void
 
@@ -10,9 +10,8 @@ export abstract class CucumberCheck {
         this.#arbitraries.set(name, a)
     }
 
-    property<U>(name: string, f: (out: this, _: U) => void) {
-        if (!this.#properties.has(name)) this.#properties.set(name, new Array)
-        this.#properties.get(name)!.push(a => f(this, a))
+    property(f: (out: this, tc: any) => void) {
+        this.#properties.push(tc => f(this, tc))
     }
 
     assert(assertion: (out: this, _: any) => void) {
@@ -21,9 +20,7 @@ export abstract class CucumberCheck {
 
         fc.assert(fc.property(suite, tc => {
             this.initialize();
-            [...this.#properties.entries()].forEach(([k, p]) => {
-                if (k in tc) p.forEach(f => f((tc as any)[k]))
-            })
+            this.#properties.forEach(f => f(this, tc))
             assertion(this, tc)
         })) 
     }
