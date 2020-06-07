3dc22c12ebddb04b0a8e01f27c517d314286ade5
Hugo Ferreira <bytter@gmail.com>
2020-06-08 00:20:27 +0100

    Type madness with chaining. Notebook.ts contains experiments that should become tests in the future.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,34 @@
 import * as fc from 'fast-check'
 
-export class CucumberCheck<A extends {}, F> {
-    constructor(public testFactory: () => F, 
-                private arbitraries: A = ({} as A), 
-                private properties: Array<(sut: F, tc: A) => void> = []) { }
+export class CucumberCheck<AS extends {}, F, TP extends {}> {
+    constructor(private testFactory: () => F = Object, 
+                private arbitraries: AS = ({} as AS), 
+                private properties: Array<(sut: F, tc: TP) => void> = []) { }
     
-    arbitrary<U, V extends string>(name: V, arbitrary: fc.Arbitrary<U>) {
-        Object.defineProperty(this.arbitraries, name, { value: arbitrary, enumerable: true })
-        return new CucumberCheck(this.testFactory, this.arbitraries as A & Record<V, U>, this.properties)
+    arbitrary<A, S extends string>(name: S, arbitrary: (arbs: AS) => fc.Arbitrary<A>) {
+        Object.defineProperty(this.arbitraries, name, { value: arbitrary(this.arbitraries), enumerable: true })
+        return new CucumberCheck<AS & Record<S, fc.Arbitrary<A>>, F, TP & Record<S, A>>(
+            this.testFactory, 
+            this.arbitraries as AS & Record<S, fc.Arbitrary<A>>, 
+            this.properties)
     }
 
-    property(property: (testObject: F, testCase: A) => void) {
-        this.properties.push(property)
+    chain<A, S extends string>(name: S, f: (arbitraryCases: TP) => fc.Arbitrary<A>) {
+        return this.arbitrary(name, () => fc.record(this.arbitraries).chain(arbitraries => f(arbitraries as TP)))
+    }
+
+    property(p: (testObject: F, testCase: TP) => void) {
+        this.properties.push(p)
         return this
     }
 
-    assert(assertion: (testObject: F, testCase: A) => void) {
+    assert(a: (testObject: F, testCase: TP) => void) {
         const suite = fc.record(this.arbitraries)
 
         fc.assert(fc.property(suite, testPoint => {
             const sut = this.testFactory()
-            this.properties.forEach(f => f(sut, testPoint as A))
-            assertion(sut, testPoint as A)
+            this.properties.forEach(p => p(sut, testPoint as TP))
+            a(sut, testPoint as TP)
         }))
     }
 }
---
+++ src/notebook.ts
@@ -0,0 +1,49 @@
+import * as fc from 'fast-check'
+import { expect } from 'chai'
+import { CucumberCheck } from '.'
+
+export class Stack<T> {
+    elements: Array<T> = []
+
+    push = (e: T) => { this.elements.push(e) }
+    pop = () => { return this.elements.pop() }
+    size = () => { return this.elements.length }
+}
+
+/*
+  Scenario: An empty stack becomes nonempty when we insert any number of elements
+    Given an empty stack
+    When we push an array of elements
+    Then the stack is not empty
+*/
+
+new CucumberCheck(() => new Stack())
+  .arbitrary('elements', () => fc.array(fc.nat(), 1, 100))
+  .property((s, as) => s.push(as.elements))
+  .assert((s, _) => expect(s.size()).gt(0))
+
+// Chains must have a name, and I think that they shouldn't replace the "original" name.
+// My rationale is that it is different from chaining an arbitrary into two other arbitraries,
+// from creating two independent arbitraries. The chained ones are *dependent*, and there
+// might be scenarios where other independent arbitraries might be needed to be addressed.
+
+new CucumberCheck()
+  .arbitrary('base', () => fc.integer())
+  .chain('test', (arb) => fc.record({ a: fc.constant(arb.base), b: fc.integer(arb.base) }))
+  .assert((_, { test }) => expect(test.a).gte(test.b))
+
+// As it stands, it also doesn't force chain to receive a function that returns a dictionary;
+// any Arbitrary will be considered valid. A tuple, for example, allows nice destructuring.
+
+new CucumberCheck(() => {})
+  .arbitrary('base', () => fc.integer())
+  .chain('pair', (arb) => fc.tuple(fc.constant(arb.base), fc.integer(arb.base)))
+  .assert((_, { pair: [a, b] }) => expect(a).gte(b))
+
+// And we can invoke multiple assertions
+const scenario = new CucumberCheck()
+  .arbitrary('base', () => fc.integer(100))
+  .chain('pair', (arb) => fc.tuple(fc.constant(arb.base), fc.integer(arb.base)))
+
+scenario.assert((_, { pair: [a, b] }) => expect(a).gte(b))
+scenario.assert((_, { base }) => expect(base).lte(100))
