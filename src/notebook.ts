5340a2808de4b93d4e01faa7c3cf680ffa31a8d9
Hugo Ferreira <bytter@gmail.com>
2020-06-13 17:19:12 +0100

    Moved new stack test back to stack's test file. Changed example SUT a bit.

--- src/notebook.ts
+++ src/notebook.ts
@@ -1,14 +1,6 @@
 import { FluentCheck } from './index'
 import { ArbitraryInteger, ArbitraryBoolean, ArbitraryReal, ArbitraryString, ArbitraryComposite, ArbitraryCollection } from './arbitraries'
 
-class Stack<T> {
-    elements: Array<T> = []
-
-    push = (e: T) => { this.elements.push(e) }
-    pop = () => { return this.elements.pop() }
-    size = () => { return this.elements.length }
-}
-
 /* TODO List
  * 
  * - Separate tests into categories
@@ -22,43 +14,4 @@ class Stack<T> {
  * - ... and then meta-PBT
  */
 
-/*
-
-new FluentCheck() 
-    .given('sut', () => 1)
-    .exists('a', new ArbitraryInteger(1))
-    .then(({ a, sut }) => a == sut)
-    .check() //?
-  
-new FluentCheck()
-    .given('sut', 1)
-    .exists('a', new ArbitraryInteger(1))
-    .then(({ a, sut }) => a == sut)
-    .check() //?
-
-new FluentCheck()
-    .given('sut', () => ({ x: 0 }))
-    .when(({ sut }) => sut.x += 1)
-    .exists('a', new ArbitraryInteger(1, 1000))
-    .then(({ a, sut }) => a == sut.x)
-    .check() //?
-        
-new FluentCheck()
-    .exists('e', new ArbitraryInteger())
-    .given('stack', () => new Stack<number>())
-    .when(({ e, stack }) => stack.push(e))
-    .then(({ stack }) => stack.size() == 1)
-    .check() //?
-    
-new ArbitraryCollection(new ArbitraryInteger(0, 10), 0, 10).sample() //?
-new ArbitraryCollection(new ArbitraryInteger(0, 10), 0, 10).pick() //?
-    */
-
-new FluentCheck()
-    .exists('arr', new ArbitraryCollection(new ArbitraryInteger(1, 100), 1, 1000))
-    .given('stack', () => new Stack<number>())
-    .when(({ arr, stack }) => arr.forEach(e => stack.push(e)))
-    .then(({ arr, stack }) => stack.size() == arr.length)
-    .check() //?
-
-new ArbitraryInteger(1, 100).sampleWithBias(6) //?
+ 
--- test/stack.test.ts
+++ test/stack.test.ts
@@ -1,12 +1,12 @@
 import { FluentCheck } from '../src/index'
-import { ArbitraryInteger } from '../src/arbitraries'
+import { ArbitraryInteger, ArbitraryCollection } from '../src/arbitraries'
 import { it } from 'mocha' 
 import { expect } from 'chai'
 
 class Stack<T> {
   elements: Array<T> = []
 
-  push = (e: T) => { this.elements.push(e) }
+  push = (...e: T[]) => { this.elements.push(...e) }
   pop = () => { return this.elements.pop() }
   size = () => { return this.elements.length }
 }
@@ -14,10 +14,19 @@ class Stack<T> {
 describe('Stack tests', () => {
   it('should push one element to the stack and have size one', () => {
     expect(new FluentCheck()
-      .exists('e', new ArbitraryInteger())
+      .forall('e', new ArbitraryInteger())
       .given('stack', () => new Stack<number>())
       .when(({ e, stack }) => stack.push(e))
       .then(({ stack }) => stack.size() == 1)
+      .check()).to.have.property('satisfiable', true)
+  })
+
+  it('should push several elements to the stack and have size equal to the number of pushed elements', () => {
+    expect(new FluentCheck()
+      .forall('es', new ArbitraryCollection(new ArbitraryInteger()))
+      .given('stack', () => new Stack<number>())
+      .when(({ es, stack }) => stack.push(...es))
+      .then(({ es, stack }) => stack.size() == es.length)
       .check()).to.have.property('satisfiable', true)
   })
 })
