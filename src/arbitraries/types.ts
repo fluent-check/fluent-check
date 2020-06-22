9bcd8f367f0e2f8694cb5dfb5fab565d9a868c1e
Rui Gonçalves <ruippeixotog@gmail.com>
2020-06-22 22:07:08 +0100

    Declare `sampleSize` in `sample` and `sampleWithBias` as optional

--- src/arbitraries/types.ts
+++ src/arbitraries/types.ts
@@ -2,9 +2,9 @@ export interface Arbitrary<A> {
   size(): ArbitrarySize
 
   pick(): FluentPick<A> | undefined
-  sample(sampleSize: number): FluentPick<A>[]
+  sample(sampleSize?: number): FluentPick<A>[]
   cornerCases(): FluentPick<A>[]
-  sampleWithBias(sampleSize: number): FluentPick<A>[]
+  sampleWithBias(sampleSize?: number): FluentPick<A>[]
 
   shrink(initial: FluentPick<A>): Arbitrary<A>
   canGenerate(pick: FluentPick<A>): boolean
