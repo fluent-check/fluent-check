513b155ee40c0b9131699d16874f9e5e0b4403ba
Hugo Sereno Ferreira <bytter@gmail.com>
2021-02-25 22:55:55 +0000

    Fixed behavior when instantiating nat with unexpected bounds.
    * When max < 0, it results in a NoArbitrary
    
    * For all other intended purposes, nat(x, y) <=> nat(max(0, x), y)

--- test/nats.test.ts
+++ test/nats.test.ts
@@ -3,11 +3,21 @@ import {it} from 'mocha'
 import {expect} from 'chai'
 
 describe('Nat tests', () => {
-  it('should return a NoArbitrary if the bounds are invalid', () => {
+  it('should return a valid range if min < 0', () => {
     expect(
       fc.scenario()
         .forall('a', fc.integer(Number.MIN_SAFE_INTEGER, -1))
         .forall('b', fc.integer(0, Number.MAX_SAFE_INTEGER))
+        .then(({a, b}) => fc.nat(a, b).sample(10).every(i => i.value >= 0))
+        .check()
+    ).to.have.property('satisfiable', true)
+  })
+
+  it('should return a NoArbitrary if max < 0', () => {
+    expect(
+      fc.scenario()
+        .forall('a', fc.integer())
+        .forall('b', fc.integer(Number.MIN_SAFE_INTEGER, -1))
         .then(({a, b}) => fc.nat(a, b) === fc.empty())
         .check()
     ).to.have.property('satisfiable', true)
