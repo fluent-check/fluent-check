cd6dde87997763ce881d9d2071d073b160618b66
Hugo Sereno Ferreira <bytter@gmail.com>
2021-02-25 21:36:57 +0000

    Strengthened the bounds check on nat, by using meta-PBT ;-)

--- test/nats.test.ts
+++ test/nats.test.ts
@@ -4,6 +4,12 @@ import {expect} from 'chai'
 
 describe('Nat tests', () => {
   it('should return a NoArbitrary if the bounds are invalid', () => {
-    expect(fc.nat(-10, 10)).to.equal(fc.empty())
+    expect(
+      fc.scenario()
+        .forall('a', fc.integer(Number.MIN_SAFE_INTEGER, -1))
+        .forall('b', fc.integer(0, Number.MAX_SAFE_INTEGER))
+        .then(({a, b}) => fc.nat(a, b) === fc.empty())
+        .check()
+    ).to.have.property('satisfiable', true)
   })
 })
