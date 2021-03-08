afe92a4b22bcec6397f18752b79f0890ee6bcad1
Tiago Castro <tiagoaraujocastro@gmail.com>
2021-03-08 17:25:06 +0000

    Added seed generation

--- src/arbitraries/util.ts
+++ src/arbitraries/util.ts
@@ -13,6 +13,14 @@ export function stringify(object: any) {
   return (object instanceof Object || object instanceof Array) ? JSON.stringify(object) : object
 }
 
+//Splitmix64 code adapted from https://prng.di.unimi.it/splitmix64.c
+export function splitmix64(x: {val: number}) {
+  let z: number = (x.val += 0x9e3779b97f4a7c15)
+  z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9
+  z = (z ^ (z >> 27)) * 0x94d049bb133111eb
+  return z ^ (z >> 31)
+}
+
 export const NilArbitrarySize: ArbitrarySize = {value: 0, type: 'exact'}
 export const significance = 0.90
 export const lowerCredibleInterval = (1 - significance) / 2
