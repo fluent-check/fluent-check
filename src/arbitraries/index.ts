3c369c5eb843e07688b10fc5d2ec63fb32d658a8
Rui Guedes <ruiguedes22@gmail.com>
2021-03-16 19:03:37 +0000

    Finished string() modification and refactored the associated code.

--- src/arbitraries/index.ts
+++ src/arbitraries/index.ts
@@ -24,27 +24,24 @@ export const real = (min = Number.MIN_SAFE_INTEGER, max = Number.MAX_SAFE_INTEGE
 export const nat = (min = 0, max = Number.MAX_SAFE_INTEGER): Arbitrary<number> =>
   max < 0 ? NoArbitrary : integer(Math.max(0, min), max)
 
-export const char = (): Arbitrary<string> =>
-  new ArbitraryInteger(0x20, 0x7e).map((v) => String.fromCodePoint(v))
+export const char = (min = 0x20, max = 0x7e): Arbitrary<string> => min < 0x20 || max > 0x7e ?
+  NoArbitrary : new ArbitraryInteger(min, max).map((v) => String.fromCodePoint(v))
 
-export const hex = (): Arbitrary<string> =>
-  new ArbitraryInteger(0, 15).map((v) => String.fromCodePoint(v < 10 ? v + 48 : v + 97 - 10))
+export const hex = (min = 0, max = 15): Arbitrary<string> => min < 0 || max > 15 ?
+  NoArbitrary : new ArbitraryInteger(min, max).map((v) => String.fromCodePoint(v < 10 ? v + 48 : v + 97 - 10))
 
-export const base64 = (): Arbitrary<string> =>
-  new ArbitraryInteger(0, 63).map((v) => String.fromCodePoint(utils.base64Mapper(v)))
+export const base64 = (min = 0, max = 63): Arbitrary<string> => min < 0 || max > 63 ?
+  NoArbitrary : new ArbitraryInteger(min, max).map((v) => String.fromCodePoint(utils.base64Mapper(v)))
 
-export const ascii = (): Arbitrary<string> =>
-  new ArbitraryInteger(0x00, 0x7f).map((v) => String.fromCodePoint(utils.printableCharactersMapper(v)))
+export const ascii = (min = 0x00, max = 0x7f): Arbitrary<string> => min < 0x00 || max > 0x7f ?
+  NoArbitrary : new ArbitraryInteger(min, max).map((v) => String.fromCodePoint(utils.printableCharactersMapper(v)))
 
 export const unicode = (encoding = 'utf-8'): Arbitrary<string> => encoding === 'utf-16' ?
   new ArbitraryInteger(0x0000, 0x10ffff).map((v) => String.fromCodePoint(utils.printableCharactersMapper(v))) :
   new ArbitraryInteger(0x0000, 0x10f7ff).map((v) => String.fromCodePoint(utils.utf8Mapper(v)))
 
-export const string = (min = 2, max = 10, chars = 'abcdefghijklmnopqrstuvwxyz'): Arbitrary<string> =>
-  chars === '' ? constant('') : array(integer(0, chars.length - 1).map(n => chars[n]), min, max).map(a => a.join(''))
-
-// export const string = (min = 2, max = 10, charArb = char()): Arbitrary<string> =>
-//   min > max ? NoArbitrary : array(charArb, min, max).map(a => a.join(''))
+export const string = (min = 2, max = 10, charArb = char()): Arbitrary<string> =>
+  min === 0 && max === 0 ? constant('') : array(charArb, min, max).map(a => a.join(''))
 
 export const array = <A>(arbitrary: Arbitrary<A>, min = 0, max = 10): Arbitrary<A[]> =>
   min > max ? NoArbitrary : new ArbitraryArray(arbitrary, min, max)
