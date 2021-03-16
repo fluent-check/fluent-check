e26468e10be11fbaa9f5849cbbc718c82e0715a8
Rui Guedes <ruiguedes22@gmail.com>
2021-03-16 19:18:48 +0000

    Added MappedArbitrary tests to test the map inverse transformation.

--- test/arbitrary.test.ts
+++ test/arbitrary.test.ts
@@ -241,16 +241,21 @@ describe('Arbitrary tests', () => {
         .filter(a => a === false)
         .map(a => a ? 0 : 1, b => b === 0 ? true : false)
         .canGenerate({original: 0, value: 0})).to.be.false
-      // TODO: (Should now be removed?) This should be false. However, we are not checking if the filter is able to
-      // actually generate the value due to missing intermediate information (i.e. multiple maps generate intermediate
-      // different values - and types - and we only preserve the root). Maybe we should consider preserving the full
-      // path.
 
       expect(fc.integer(0, 1)
         .map(a => a === 1, b => b ? 1 : 0)
         .filter(a => a === false)
         .map(a => a ? 0 : 1, b => b === 0 ? true : false)
         .canGenerate({original: 1, value: 1})).to.be.true
+    })
+  })
+
+  describe('Mapped Arbitraries', () => {
+    it('should be able to inverse map transformation', () => {
+      expect(fc.integer(-10, 0).map(a => Math.abs(a), b => -b)
+        .canGenerate({original: -5, value: -5})).to.be.false
+      expect(fc.integer(-10, 10).map(a => Math.abs(a), b => [-b, b])
+        .canGenerate({original: -5, value: -5})).to.be.true
     })
   })
 
