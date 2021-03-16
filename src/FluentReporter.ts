81d60567c957e909b6faa26bae783b91fa6f7acb
Tiago Castro <tiagoaraujocastro@gmail.com>
2021-03-16 17:34:21 +0000

    Reporter function now outputs data in a clear way

--- src/FluentReporter.ts
+++ src/FluentReporter.ts
@@ -2,6 +2,18 @@ import {FluentResult} from './FluentCheck'
 
 export function expect(result: FluentResult): void | never {
   if (!result.satisfiable) {
-    throw (result)
+    throw new FluentReporter(result)
+  }
+}
+
+class FluentReporter extends Error {
+  constructor(result: FluentResult) {
+    super()
+    this.name = 'Property not satisfiable'
+
+    const msg: String[] = []
+    msg.push('\n\nCounter-example:\n')
+    msg.push(JSON.stringify(result.example))
+    this.message = msg.join('')
   }
 }
