e2656e22e3eed880106fb956650b345ae47400e7
Rui Guedes <ruiguedes22@gmail.com>
2021-03-27 20:38:35 +0000

    Comment code that should not be running on the pipeline.

--- test/debug.test.ts
+++ test/debug.test.ts
@@ -1,18 +1,18 @@
-import * as fc from '../src/index'
-import {it} from 'mocha'
-import {expect} from 'chai'
-import {MySum} from '../src/externalClass'
+// import * as fc from '../src/index'
+// import {it} from 'mocha'
+// import {expect} from 'chai'
+// import {MySum} from '../src/externalClass'
 
-describe('Debug tests', () => {
-  it('Testing coverage', () => {
-    expect(fc.scenario()
-      .config(fc.strategy().defaultStrategy().withCoverageGuidance('test/debug.test.ts'))
-      .forall('a', fc.integer())
-      .forall('b', fc.integer())
-      .then(({a, b}) => {
-        const tmp = new MySum()
-        return tmp.mySum(a,b) === a + b
-      })
-      .check()).to.have.property('satisfiable', true)
-  })
-})
+// describe('Debug tests', () => {
+//   it('Testing coverage', () => {
+//     expect(fc.scenario()
+//       .config(fc.strategy().defaultStrategy().withCoverageGuidance('test/debug.test.ts'))
+//       .forall('a', fc.integer())
+//       .forall('b', fc.integer())
+//       .then(({a, b}) => {
+//         const tmp = new MySum()
+//         return tmp.mySum(a,b) === a + b
+//       })
+//       .check()).to.have.property('satisfiable', true)
+//   })
+// })
