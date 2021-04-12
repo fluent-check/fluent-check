02be944545a929e9f088b76e48783dc20510d4e6
Tiago Castro <tiagoaraujocastro@gmail.com>
2021-04-12 18:40:02 +0100

    Added fix for bug

--- src/arbitraries/ArbitraryReal.ts
+++ src/arbitraries/ArbitraryReal.ts
@@ -1,8 +1,14 @@
 import {ArbitraryInteger} from './internal'
+import { ArbitrarySize } from './types'
 
 export class ArbitraryReal extends ArbitraryInteger {
   constructor(public min = Number.MIN_SAFE_INTEGER, public max = Number.MAX_SAFE_INTEGER) {
     super(min, max)
+  }
+
+  size(): ArbitrarySize {
+    const value = Number.MAX_SAFE_INTEGER
+    return {value, type: 'exact', credibleInterval: [value, value]}
   }
 
   pick(generator: () => number)  {
