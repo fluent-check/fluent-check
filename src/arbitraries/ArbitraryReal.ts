f0271590c04780a42c66c69b554b0c54042cba29
Tiago Castro <tiagoaraujocastro@gmail.com>
2021-04-12 18:34:35 +0100

    Improved capped arbitrary size fix

--- src/arbitraries/ArbitraryReal.ts
+++ src/arbitraries/ArbitraryReal.ts
@@ -6,8 +6,8 @@ export class ArbitraryReal extends ArbitraryInteger {
     super(min, max)
   }
 
-  size(precision = 10): ArbitrarySize {
-    const value = 1 + (this.max - this.min) * 10 ** precision
+  size(precision?: number): ArbitrarySize {
+    const value = precision === undefined ? Number.MAX_SAFE_INTEGER : 1 + (this.max - this.min) * 10 ** precision
     return {value, type: 'exact', credibleInterval: [value, value]}
   }
 
