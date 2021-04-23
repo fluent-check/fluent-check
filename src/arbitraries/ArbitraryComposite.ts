aead1fa6f4ac12996c2c33a3db47ce4d1c5f865b
Rui Guedes <ruiguedes22@gmail.com>
2021-04-23 19:21:34 +0100

    Implemented the ArbitraryComposite mutate function.

--- src/arbitraries/ArbitraryComposite.ts
+++ src/arbitraries/ArbitraryComposite.ts
@@ -1,6 +1,6 @@
 import {Arbitrary} from './internal'
 import {FluentPick} from './types'
-import {mapArbitrarySize, NilArbitrarySize} from './util'
+import {mapArbitrarySize, NilArbitrarySize, getRandomInt} from './util'
 import * as fc from './index'
 
 export class ArbitraryComposite<A> extends Arbitrary<A> {
@@ -35,6 +35,26 @@ export class ArbitraryComposite<A> extends Arbitrary<A> {
 
   canGenerate(pick: FluentPick<A>) {
     return this.arbitraries.some(a => a.canGenerate(pick))
+  }
+
+  mutate(pick: FluentPick<A>, generator: () => number, maxNumMutations: number): FluentPick<A>[] {
+    const result: FluentPick<A>[] = []
+    const baseArbitrary = this.arbitraries.find(x => x.canGenerate(pick))
+
+    if (baseArbitrary === undefined) return result
+
+    const baseArbitrarySize = baseArbitrary.size()
+    const numMutations = baseArbitrarySize.type === 'exact' ?
+      Math.min(baseArbitrarySize.value - 1, getRandomInt(1, maxNumMutations, generator)) :
+      getRandomInt(1, maxNumMutations, generator)
+
+    while (result.length < numMutations) {
+      const mutatedPick = baseArbitrary.mutate(pick, generator, 1)[0]
+      if (baseArbitrary.canGenerate(mutatedPick) && result.every(x => x.value !== mutatedPick.value))
+        result.push(mutatedPick)
+    }
+
+    return result
   }
 
   toString(depth = 0) {
