280b09e0a0271f17895b7d6c7387a0ed748f0c60
Rui Guedes <ruiguedes22@gmail.com>
2021-04-23 18:41:50 +0100

    Implemented the WrappedArbitrary mutate function.

--- src/arbitraries/WrappedArbitrary.ts
+++ src/arbitraries/WrappedArbitrary.ts
@@ -14,6 +14,10 @@ export class WrappedArbitrary<A> extends Arbitrary<A> {
     return this.baseArbitrary.canGenerate(pick)
   }
 
+  mutate(pick: FluentPick<A>, generator: () => number, maxNumMutations: number): FluentPick<A>[] {
+    return this.baseArbitrary.mutate(pick, generator, maxNumMutations)
+  }
+
   toString(depth = 0) {
     return ' '.repeat(depth * 2) +
       'Wrapped Arbitrary:\n' + this.baseArbitrary.toString(depth + 1)
