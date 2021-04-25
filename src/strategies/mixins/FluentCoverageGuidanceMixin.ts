d9f4a12dbb6933c416707da4486334bd738d935b
Rui Guedes <ruiguedes22@gmail.com>
2021-04-25 15:13:02 +0100

    Ensured that each coverage-guided created collection does not contain duplicates.

--- src/strategies/mixins/FluentCoverageGuidanceMixin.ts
+++ src/strategies/mixins/FluentCoverageGuidanceMixin.ts
@@ -6,7 +6,7 @@ import * as libInstrument from 'istanbul-lib-instrument'
 import {performance} from 'perf_hooks'
 import {FluentCoverage} from '../FluentCoverage'
 import {MixinStrategy} from '../FluentStrategyTypes'
-import {WrapFluentPick} from '../../arbitraries'
+import {FluentPick, WrapFluentPick} from '../../arbitraries'
 import {FluentStrategyInterface} from '../FluentStrategy'
 
 export function CoverageGuidance<TBase extends MixinStrategy>(Base: TBase) {
@@ -106,10 +106,17 @@ export function CoverageGuidance<TBase extends MixinStrategy>(Base: TBase) {
       else if (this.testCaseCollectionPick >= this.testCaseCollection.length) {
         for (const name in this.arbitraries) {
           this.arbitraries[name].collection = []
-          for (const input of this.arbitraries[name].seedCollection)
-            this.arbitraries[name].collection.push(... this.arbitraries[name].arbitrary
-              .mutate(input, this.randomGenerator.generator, this.configuration.maxNumMutations))
-          // TODO - Make sure that the arbitrary collection contains unique values
+          const collectionMap: Map<string, FluentPick<any>> = new Map()
+
+          for (const input of this.arbitraries[name].seedCollection) {
+            const inputMutations = this.arbitraries[name].arbitrary
+              .mutate(input, this.randomGenerator.generator, this.configuration.maxNumMutations)
+
+            for (const mutatedInput of inputMutations)
+              if (!collectionMap.has(JSON.stringify(mutatedInput.value)))
+                collectionMap.set(JSON.stringify(mutatedInput.value), mutatedInput)
+          }
+          this.arbitraries[name].collection.push(... Array.from(collectionMap.values()))
         }
         this.testCaseCollectionPick = 0
         this.testCaseCollectionMutationStatus = true
