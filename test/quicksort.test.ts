c7f7b4b804193f2274b8fd14be211e8481d551cd
Tiago Castro <tiagoaraujocastro@gmail.com>
2021-05-19 20:20:52 +0100

    Added quicksort function without errors

---
+++ test/quicksort.test.ts
@@ -0,0 +1,57 @@
+import * as fc from '../src/index'
+import {it} from 'mocha'
+
+function swap(items, leftIndex, rightIndex) {
+  const temp = items[leftIndex]
+  items[leftIndex] = items[rightIndex]
+  items[rightIndex] = temp
+}
+
+function partition(items, left, right) {
+  const pivot = items[Math.floor((right + left) / 2)]
+  let i = left
+  let j = right
+  while (i <= j) {
+    while (items[i] < pivot)
+      i++
+    while (items[j] > pivot)
+      j--
+    if (i <= j) {
+      swap(items, i, j)
+      i++
+      j--
+    }
+  }
+  return i
+}
+
+function quickSort(items, left, right) {
+  if (items.length > 1) {
+    const index = partition(items, left, right)
+    if (left < index - 1)
+      quickSort(items, left, index - 1)
+    if (index < right)
+      quickSort(items, index, right)
+  }
+  return items
+}
+
+console.log(quickSort([], 0, 0))  //for no lint errors
+
+describe('Section examples', () => {
+  let seededGen: (seed: number) => () => number
+
+  beforeEach(() =>
+    seededGen = (seed: number) => () => (seed = seed * 16807 % 2147483647) / 2147483647
+  )
+
+  it('Property', () => {
+    fc.expect(fc.scenario()
+      //.configStatistics(fc.statistics().withAll().withDefaultGraphs())
+      .withGenerator(seededGen)
+      .forall('a', fc.integer(-10,10))
+      .then(({a}) => a === a)
+      .check()
+    )
+  })
+})
