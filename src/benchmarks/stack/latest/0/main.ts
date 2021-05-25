97afc4132b72a5a8ca24f466b0bae46f92961892
Rui Guedes <ruiguedes22@gmail.com>
2021-05-25 10:57:41 +0100

    Removed template benchmark.

--- src/benchmarks/stack/latest/0/main.ts
+++
@@ -1,7 +0,0 @@
-export class Stack<T> {
-  elements: Array<T> = []
-
-  push = (..._: T[]) => {}
-  pop = () => { return this.elements.pop() }
-  size = () => { return this.elements.length }
-}
--- src/benchmarks/stack/latest/1/main.ts
+++
@@ -1,7 +0,0 @@
-export class Stack<T> {
-  elements: Array<T> = []
-
-  push = (...e: T[]) => { this.elements.push(...e) }
-  pop = () => {}
-  size = () => { return this.elements.length }
-}
