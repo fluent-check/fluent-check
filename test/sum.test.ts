90351131d38a2025e167160ed1d38f53e21bf135
Tiago Castro <tiagoaraujocastro@gmail.com>
2021-05-28 18:01:04 +0100

    Added sum.test.ts containing sum function & properties for examplifying

---
+++ test/sum.test.ts
@@ -0,0 +1,69 @@
+import * as fc from '../src/index'
+import {it} from 'mocha'
+
+// Code under test
+const inc = (x: number): number => x + 1
+const dec = (x: number): number => x - 1
+const sum = (a: number, b: number): number => {
+  if (b > 0)
+    for (let i = 0; i < b; i++)
+      a = inc(a)
+  else
+    for (let i = 0; i < -b; i++)
+      a = dec(a)
+  return a
+}
+
+// Sum Properties
+describe('sum', () => {
+  // Comutative property
+  it('Comutative property of addition', () => {
+    fc.expect(fc.scenario()
+      .forall('a', fc.integer(-100, 100))
+      .forall('b', fc.integer(-100, 100))
+      .then(({a, b}) => sum(a,b) === sum(b,a))
+      .check()
+    )
+  })
+
+  // Associative property
+  it('Associative property of addition', () => {
+    fc.expect(fc.scenario()
+      .config(fc.strategy().defaultStrategy().withSampleSize(50))
+      .forall('a', fc.integer(-100, 100))
+      .forall('b', fc.integer(-100, 100))
+      .forall('c', fc.integer(-100, 100))
+      .then(({a, b, c}) => sum(sum(a,b), c) === sum(a, sum(b,c)))
+      .check()
+    )
+  })
+
+  // Identity property
+  it('Identity property of addition', () => {
+    fc.expect(fc.scenario()
+      .forall('a', fc.integer(-100, 100))
+      .then(({a}) => sum(a,0) === a)
+      .check()
+    )
+  })
+
+  // Closure property
+  it('Closure property of addition', () => {
+    fc.expect(fc.scenario()
+      .forall('a', fc.integer(-100, 100))
+      .forall('b', fc.integer(-100, 100))
+      .then(({a, b}) => Number.isInteger(sum(a,b)))
+      .check()
+    )
+  })
+
+  // Extra property
+  it('Extra property of addition', () => {
+    fc.expect(fc.scenario()
+      .forall('a', fc.integer(-100, 100))
+      .forall('b', fc.integer(-100, 100))
+      .then(({a, b}) => sum(a,b) === sum(a-1,b+1))
+      .check()
+    )
+  })
+})
